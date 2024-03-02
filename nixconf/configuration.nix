2352d4ee27f5c4ab1deb97faf32cd7317e40c32f
iKramp <nejc.susnik05@gmail.com>
2024-03-02 15:57:11 +0100

    added nixos configuration

---
+++ nixconf/configuration.nix
@@ -0,0 +1,133 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "abacus_nixos"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Ljubljana";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sl_SI.UTF-8";
+    LC_IDENTIFICATION = "sl_SI.UTF-8";
+    LC_MEASUREMENT = "sl_SI.UTF-8";
+    LC_MONETARY = "sl_SI.UTF-8";
+    LC_NAME = "sl_SI.UTF-8";
+    LC_NUMERIC = "sl_SI.UTF-8";
+    LC_PAPER = "sl_SI.UTF-8";
+    LC_TELEPHONE = "sl_SI.UTF-8";
+    LC_TIME = "sl_SI.UTF-8";
+  };
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nejc = {
+    isNormalUser = true;
+    description = "Nejc";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+
+  
+  environment.systemPackages = with pkgs; [
+    neovim
+    kitty
+    firefox
+    lf
+    rofi
+    waybar
+    rustup
+    spotify
+    steam
+    stow
+    deepin.dde-file-manager
+    htop
+    wireplumber
+    pipewire
+    vlc
+    unstable.spacedrive
+    git
+    pavucontrol
+    playerctl
+    vesktop
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  programs.hyprland.enable = true;
+  programs.zsh.enable = true;
+
+  #make electron apps work
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
