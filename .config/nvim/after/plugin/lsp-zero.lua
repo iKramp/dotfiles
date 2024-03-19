7f91f934df6cd7dd72ea8229ba32459d54bfb8c7
iKramp <nejc.susnik05@gmail.com>
2024-03-19 20:29:32 +0100

    some things

--- .config/nvim/after/plugin/lsp-zero.lua
+++ .config/nvim/after/plugin/lsp-zero.lua
@@ -1,5 +1,11 @@
 local lsp_zero = require('lsp-zero')
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
@@ -24,6 +30,7 @@ lsp_zero.setup_servers({'clangd', 'rust_analyzer', 'lua_ls'})
 lsp_zero.extend_cmp()
 
 local cmp = require('cmp')
+local luasnip = require("luasnip")
 
 cmp.setup({
   sources = {
@@ -36,10 +43,28 @@ cmp.setup({
 --  }),
   mapping = {
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = cmp.mapping.confirm({select = true}),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ['<Escape>'] = cmp.mapping.abort(),
-    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
   },
   snippet = {
     expand = function(args)
