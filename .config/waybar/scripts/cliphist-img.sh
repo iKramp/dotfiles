109f5a801688b81424edbefd9338373265e3a067
Nejc <nejc.susnik05@gmail.com>
2025-01-14 11:44:15 +0100

    cliphist

--- .config/waybar/scripts/cliphist-img.sh
+++
@@ -1,1 +0,0 @@
-rofi -modi clipboard:~/dotfiles/.config/waybar/scripts/cliphist-rofi-img -show clipboard -replace -config ~/dotfiles/.config/rofi/rofi-cliphist-img.rasi
---
+++ scripts/cliphist-img.sh
@@ -0,0 +1,1 @@
+rofi -modi clipboard:~/dotfiles/scripts/cliphist-rofi-img.sh -show clipboard -replace -config ~/dotfiles/.config/rofi/rofi-cliphist-img.rasi
