d34965f5a31edb6562d28b6b0aadabcf50e98b69
Nejc Sušnik <nejc.susnik05@gmail.com>
2025-01-15 20:03:57 +0100

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
-sudo nix-collect-garbage --delete-older-than 2d
-sudo nixos-rebuild switch
-sudo nix-store --optimise
+#sudo nix-collect-garbage --delete-older-than 2d
+#sudo nixos-rebuild switch
+#sudo nix-store --optimise
+echo "Not working rn"
