2581affb4a98b431241bb04f8e4a418f1908de00
Nejc Sušnik <nejc.susnik05@gmail.com>
2025-01-15 11:16:00 +0100

    multi monitor

--- scripts/set_wallpaper.sh
+++ scripts/set_wallpaper.sh
@@ -2,8 +2,9 @@
 
 CURR=$(hyprctl hyprpaper listloaded)
 hyprctl hyprpaper preload "$1"
-hyprctl hyprpaper wallpaper "DP-1,$1"
-hyprctl hyprpaper wallpaper "eDP-1,$1"
-hyprctl hyprpaper wallpaper "HDMI-A-1,$1"
+#hyprctl hyprpaper wallpaper "DP-1,$1"
+#hyprctl hyprpaper wallpaper "eDP-1,$1"
+#hyprctl hyprpaper wallpaper "HDMI-A-1,$1"
+hyprctl hyprpaper wallpaper ",$1"
 hyprctl hyprpaper unload "$CURR"
 
