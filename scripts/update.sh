49aeefb3eb8bdd4bce561e3c6a1ea5a1e5045a2c
Nejc Sušnik <nejc.susnik05@gmail.com>
2025-12-10 12:41:53 +0100

    dots

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,9 @@
 cd ~/dotfiles/nixconf
 sudo nix-collect-garbage --delete-older-than 2d
-if [[ "$2" == "-uu" ]]; then
+if [[ "$2" == "-u" ]]; then
     sudo nix flake update
-    sudo nixos-rebuild switch --flake ./#"$1" --upgrade
-elif [[ "$2" == "-u" ]]; then
-    sudo nixos-rebuild switch --flake ./#"$1" --upgrade
+    sudo nixos-rebuild switch --flake ./#"$1" -vv
 else
-    sudo nixos-rebuild switch --flake ./#"$1"
+    sudo nixos-rebuild switch --flake ./#"$1" -vv
 fi
 sudo nix-store --optimise
