df480457b7e81d5fa16b1d69e3e4efc7c502bcdc
Nejc <nejc.susnik05@gmail.com>
2026-01-01 19:40:31 +0100

    tap0

--- nixconf/configuration.nix
+++ nixconf/configuration.nix
@@ -26,7 +26,7 @@
   # Needed for qemu
   networking.interfaces.tap0 = {
     ipv4.addresses = [{
-      address = "192.168.178.1";
+      address = "10.0.0.0";
       prefixLength = 24;
     }];
     virtual = true;
